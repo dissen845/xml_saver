498fb4da7fd5944424aac3c9142180c3e811fe66
dissen <bleckmesa@mail.ru>
2026-05-09 12:07:16 +0300

    Update: добавлен тест хелзчека;

--- file_manager/tests.py
+++ file_manager/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HealthCheckTest(TestCase):
+
+    def test_health_returns_200_and_ok(self):
+        response = self.client.get("/health_check/")
+        self.assertEqual(response.status_code, 200)
+        self.assertJSONEqual(response.content, {"status": "ok"})
