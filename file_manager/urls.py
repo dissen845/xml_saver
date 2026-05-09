4a2346b08484aa3e9b6d7e515dd289e806b9eb95
dissen <bleckmesa@mail.ru>
2026-05-09 15:12:49 +0300

    Update: добавляем новое api;

--- file_manager/urls.py
+++ file_manager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import health_check
+from .views import health_check, upload_xml
 
 
 urlpatterns = [
     path("health_check/", health_check, name="health_check"),
+    path("upload/xml/", upload_xml, name="upload_xml"),
 ]
--- file_manager/views.py
+++ file_manager/views.py
@@ -1,7 +1,11 @@
-from django.http import JsonResponse
+from django.http import HttpRequest, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_POST, require_GET
+from .services.xml_service import XMLFileService, XMLFileValidationError
 
 
-def health_check(_):
+@require_GET
+def health_check(_) -> JsonResponse:
     """
     Простой healthcheck: всегда отвечает 200 OK
     и сообщает, что сервис работает.
@@ -9,3 +13,33 @@ def health_check(_):
 
     data = {"status": "ok"}
     return JsonResponse(data)
+
+
+@require_POST
+@csrf_exempt
+def upload_xml(request: HttpRequest) -> JsonResponse:
+    """
+    Принимает POST-запрос с XML-файлом в поле "file".
+    Сохраняет файл и возвращает JSON с информацией о сохранённом объекте.
+    """
+
+    # Проверяем, что файл передан в запросе
+    if "file" not in request.FILES:
+        return JsonResponse(status=400, data={"error": "Не передан файл"})
+
+    uploaded_file = request.FILES["file"]
+
+    try:
+        xml_file = XMLFileService.process_xml_upload(uploaded_file)
+        file_info = XMLFileService.get_file_info(xml_file)
+        return JsonResponse(file_info, status=201)
+
+    except XMLFileValidationError as e:
+        return JsonResponse({"error": str(e)}, status=400)
+
+    # Неожиданная ошибка
+    except Exception as e:
+        return JsonResponse(
+            {"error": "Внутренняя ошибка сервера", "detail": str(e)},
+            status=500
+        )
