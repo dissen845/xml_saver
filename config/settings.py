527f5e1daf6be06fe35865ae9c3a1b12e1248dca
dissen <bleckmesa@mail.ru>
2026-05-09 13:24:11 +0300

    Update: для начала сделаем просто папку, куда сохраняем файлы;

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,13 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Папка, куда будут физически сохраняться файлы
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# URL-префикс, по которому файлы будут доступны в браузере
+MEDIA_URL = "/media/"
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
