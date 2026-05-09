cd99da095a0016e1abcdd2db6148c24c8c779a6a
dissen <bleckmesa@mail.ru>
2026-05-09 13:24:48 +0300

    Update: добавление ORM модели для хранения информации о файле;

--- file_manager/models.py
+++ file_manager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class XMLFile(models.Model):
+    """
+    Модель для хранения информации о загруженном XML-файле.
+    """
+
+    # Поле для хранения файла. upload_to – подпапка внутри MEDIA_ROOT
+    file = models.FileField(upload_to="xml_files/")
+    original_name = models.CharField(max_length=255)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    # TODO Если в будущем планируется обработка XML, можно добавить поле статуса
+    # processed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.original_name
